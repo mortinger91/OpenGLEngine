4f7a0cd7da6e3d1745e9303535577bc823899b43
mortinger91 <micheleemiliani91@gmail.com>
2023-01-09 22:34:33 +0100

    fixed cmake argument

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -7,7 +7,7 @@ mkdir release
 cp ../imgui.ini debug/
 cp ../imgui.ini release/
 cd debug
-cmake -D CMAKE_BUILD_TYPE=DEBUG ../..
+cmake -DCMAKE_BUILD_TYPE=DEBUG ../..
 cd ..
 cd release
-cmake -D CMAKE_BUILD_TYPE=RELEASE ../..
+cmake -DCMAKE_BUILD_TYPE=RELEASE ../..
